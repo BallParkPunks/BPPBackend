e2f335a136e8201d1a9fd0a2ba91732b572959a2
Adrian Lawson <adrianglawson@gmail.com>
2023-03-07 07:58:58 -0800

    add GetPrice for easy data retrieval

---
+++ functions/GetPrice.js
@@ -0,0 +1,15 @@
+import { ethers } from 'ethers'
+import { GetProvider } from './GetProvider.js'
+import { nft_abi } from '../abi.js'
+
+export const GetPrice = async (req) => {
+    const type = req.query.type
+    const amount = req.query.amount
+
+    const provider = process.env.state === 'testing' ? GetProvider('sepolia') : GetProvider('polygon')
+    const bpp = new ethers.Contract(process.env.bpp_contract, nft_abi, provider)
+
+    const price = await bpp.getPrice(type, amount)
+
+    return parseInt(price)
+}
