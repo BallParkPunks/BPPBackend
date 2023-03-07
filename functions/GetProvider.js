0efe2be11999102c9c81d2c3b9fd0d599effa18d
Adrian Lawson <adrianglawson@gmail.com>
2023-03-07 07:58:21 -0800

    add Sepolia testnet

--- functions/GetProvider.js
+++ functions/GetProvider.js
@@ -4,6 +4,7 @@ export const GetProvider = (network) => {
     const providers = {
         'goerli': process.env.RPC_GOERLI,
         'polygon': process.env.RPC_POLYGON,
+        'sepolia': process.env.RPC_SEPOLIA
     }
 
     return new ethers.providers.JsonRpcProvider(providers[network])
